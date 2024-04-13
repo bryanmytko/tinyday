d0c8c77aacaa2453403c2632b6cee03f5ee4766d
Bryan Mytko <bryanmytko@gmail.com>
2024-04-13 18:23:59 -0400

    Remove old favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "tinyday.",
   description: "",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({
